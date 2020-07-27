7b13db59af7fdbf4c70d4a0f2670d7b2215d7f49
mariaYunB <maria.yun.b@gmail.com>
2020-07-27 14:30:46 +0300

    created js-file with data

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    "brand": "BMW",
+    "model": "5 series",
+    "min_price": 75000,
+    "images": [
+      "./img/car-bmw.jpg",
+      "./img/car-bmw.jpg",
+      "./img/car-bmw.jpg"
+    ],
+    "mileage": {
+      "month": 4000,
+      "half-year": 25000,
+      "year": 60000
+    },
+    "characteristics": {
+      "type": "sedan",
+      "engine": "diesel",
+      "power": 250
+    }
+  }
+]
