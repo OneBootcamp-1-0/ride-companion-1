486cf862bcd0f85a94fbe9d434b9907967c8b9d1
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-07-27 19:42:50 +0500

    reusable carousel, added case when the length of imgs is one

--- js/index.js
+++ js/index.js
@@ -16,16 +16,16 @@ const characteristicsRu = {
   }
 };
 
-const makeImages = (imgaes, brand, model) => imgaes.map(url =>
-  `<img src="${url}" class="carousel__img" alt="${brand} ${model}" height="135px" width="255px">`
+const makeImages = (images, imgAlt) => images.map(url =>
+  `<img src="${url}" class=${images.length === 1 ? 'carousel__img--single' : 'carousel__img'} alt="${imgAlt}" height="135px" width="255px">`
 ).join('');
 
-const createCarouselElement = (images, brand, model) => {
+const createCarouselElement = (images, imgAlt) => {
   return `<div class="carousel">
-    <div class="carousel__list">
-      ${makeImages(images, brand, model)}
+    <div class="${images.length === 1 ? "carousel__list carousel__list--single" : "carousel__list"}">
+      ${makeImages(images, imgAlt)}
     </div>
-    <button class="carousel__btn carousel__btn--left" type="button" aria-label="предыдущий слайд">
+    ${images.length === 1 ? '' : `<button class="carousel__btn carousel__btn--left" type="button" aria-label="предыдущий слайд">
       <svg class="carousel__arrow" width="18" height="82" viewBox="0 0 18 82" fill="white"  stroke="#E0E0E0" xmlns="http://www.w3.org/2000/svg">
         <line x1="15.9391" y1="0.343582" x2="0.939123" y2="41.3436" stroke-width="2"/>
         <line x1="16.0626" y1="81.343" x2="1.0592" y2="40.3443" stroke-width="2"/>
@@ -36,14 +36,13 @@ const createCarouselElement = (images, brand, model) => {
         <line y1="-1" x2="43.6575" y2="-1" transform="matrix(0.343547 0.939135 0.93911 -0.343616 2.0016 0)" stroke-width="2"/>
         <line y1="-1" x2="43.6575" y2="-1" transform="matrix(0.343625 -0.939107 -0.939082 -0.343694 0.000106812 81)" stroke-width="2"/>
       </svg>
-    </button>
+    </button>`}
   </div>`
 };
-
 const createCarElement = ({ brand, model, images, minPrice, mileage, characteristics }) => `<article class="catalog-item">
   <h3 class="catalog-item__title"><a href="#">${brand} ${model}</a></h3>
   <p class="catalog-item__price">от ${minPrice} ₽ / мес.</p>
-  ${createCarouselElement(images, brand, model)}
+  ${createCarouselElement(images, `${brand} ${model}`)}
   <dl class="catalog-item__mileage">
     <dt class="catalog-item__mileage-title">Пробег</dt>
     ${!mileage.month ? '' : `<dd class="catalog-item__milage-tariff">в месяц ${mileage.month} км,</dd>`}
