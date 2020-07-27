1431b46ef910a32cb837698e1fa9aebdc9677cf4
mariaYunB <maria.yun.b@gmail.com>
2020-07-27 14:24:10 +0300

    creating of html template

---
+++ js/index.js
@@ -0,0 +1,57 @@
+const createTemplate = ({brand, model, min_price, images, mileage, characteristics}) => {
+  const characteristicsRu = {
+    type: {
+      any: 'любой',
+      sedan: 'седан',
+      estate: 'универсал',
+      hatchback: 'хэтчбек',
+      suv: 'внедорожник',
+      coupe: 'купе',
+      cabriolet: 'кабриолет'
+    },
+    engine: {
+      all: 'все',
+      petrol: 'бензин',
+      diesel: 'дизель',
+      electric: 'электро'
+    }
+  }
+  const makeImages = () => {
+    return images.map((img, i) => {
+      return `<img src="${img}" class="${i === 1 ? "" : "carousel__img"}" alt="${brand} ${model}" height="135px" width="255px">`
+    }).join('')
+  }
+  const car = document.createElement('article')
+  car.classList.add('catalog-item')
+  car.innerHTML = `<h3 class="catalog-item__title"><a href="#">${brand} ${model}</a></h3>
+  <p class="catalog-item__price">от ${min_price} ₽ / мес.</p>
+  <div class="carousel">
+    <div class="carousel__list">
+      ${makeImages()}
+    </div>
+    <button class="carousel__btn carousel__btn--left" type="button" aria-label="предыдущий слайд">
+      <svg class="carousel__arrow" width="18" height="82" viewBox="0 0 18 82" fill="white"  stroke="#E0E0E0" xmlns="http://www.w3.org/2000/svg">
+        <line x1="15.9391" y1="0.343582" x2="0.939123" y2="41.3436" stroke-width="2"/>
+        <line x1="16.0626" y1="81.343" x2="1.0592" y2="40.3443" stroke-width="2"/>
+      </svg>
+    </button>
+    <button class="carousel__btn carousel__btn--right" type="button" aria-label="следующий слайд">
+      <svg class="carousel__arrow" width="17" height="82" viewBox="0 0 17 82" fill="white"  stroke="#E0E0E0" xmlns="http://www.w3.org/2000/svg">
+        <line y1="-1" x2="43.6575" y2="-1" transform="matrix(0.343547 0.939135 0.93911 -0.343616 2.0016 0)" stroke-width="2"/>
+        <line y1="-1" x2="43.6575" y2="-1" transform="matrix(0.343625 -0.939107 -0.939082 -0.343694 0.000106812 81)" stroke-width="2"/>
+      </svg>
+    </button>
+  </div>
+  <dl class="catalog-item__mileage">
+    <dt class="catalog-item__mileage-title">Пробег</dt>
+    <dd class="catalog-item__milage-tariff">в месяц ${mileage.month} км,</dd>
+    <dd class="catalog-item__milage-tariff">полгода ${mileage['half-year']} км,</dd>
+    <dd class="catalog-item__milage-tariff">год ${mileage.year} км</dd>
+  </dl>
+  <ul class="catalog-item__characteristics">
+    <li>${characteristicsRu.type[characteristics.type]}</li>
+    <li>${characteristicsRu.engine[characteristics.engine]}</li>
+    <li>${characteristics.power} л.с.</li>
+  </ul>`
+  return car
+}
