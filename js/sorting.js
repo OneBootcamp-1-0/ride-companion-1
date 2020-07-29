1365802e750a0b673f4c0265a1283c2127dac70f
mariaYunB <maria.yun.b@gmail.com>
2020-07-29 13:06:49 +0300

    sorting

---
+++ js/sorting.js
@@ -0,0 +1,46 @@
+const radioButtons = document.querySelectorAll('input[name=sort]');
+
+radioButtons.forEach( (button) => {
+  button.addEventListener('click', () => {
+    switch (button.id) {
+      case 'recommended': {
+        renderTemplate(data);
+        break;
+      }
+      case 'expensive': {
+        const newData = [...data];
+        newData.sort( (a, b) => {
+          return b.minPrice-a.minPrice;
+        });
+        renderTemplate(newData);
+        break;
+      }
+      case 'cheap': {
+        const newData = [...data];
+        newData.sort( (a, b) => {
+          return a.minPrice-b.minPrice;
+        });
+        renderTemplate(newData);
+        break;
+      }
+      case 'economic': {
+        const newData = [...data];
+        newData.sort( (a, b) => {
+          return a.characteristics.power-b.characteristics.power;
+        });
+        renderTemplate(newData);
+        break;
+      }
+      case 'powerful': {
+        const newData = [...data];
+        newData.sort( (a, b) => {
+          return b.characteristics.power-a.characteristics.power;
+        });
+        renderTemplate(newData);
+        break;
+      }
+      default:
+        return;
+    }
+  })
+})
