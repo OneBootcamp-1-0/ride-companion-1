503555f9dd2f45cb8a2132f4187a670164a92ad8
mariaYunB <maria.yun.b@gmail.com>
2020-07-29 18:20:08 +0300

    moved from switch newdata, changed default branch, added condition for rendertemplate

--- js/sorting.js
+++ js/sorting.js
@@ -1,46 +1,38 @@
 const radioButtons = document.querySelectorAll(`input[name=sort]`);
+const newData = [...data];
 
 radioButtons.forEach( (button) => {
   button.addEventListener(`click`, () => {
     switch (button.id) {
-      case `recommended`: {
-        renderTemplate(data);
-        break;
-      }
       case `expensive`: {
-        const newData = [...data];
         newData.sort( (a, b) => {
           return b.minPrice-a.minPrice;
         });
-        renderTemplate(newData);
         break;
       }
       case `cheap`: {
-        const newData = [...data];
         newData.sort( (a, b) => {
           return a.minPrice-b.minPrice;
         });
-        renderTemplate(newData);
         break;
       }
       case `economic`: {
-        const newData = [...data];
         newData.sort( (a, b) => {
           return a.characteristics.power-b.characteristics.power;
         });
-        renderTemplate(newData);
         break;
       }
       case `powerful`: {
-        const newData = [...data];
         newData.sort( (a, b) => {
           return b.characteristics.power-a.characteristics.power;
         });
-        renderTemplate(newData);
         break;
       }
       default:
-        return;
+        renderTemplate(data);
+    }
+    if (button.id !== `recommended`) {
+      renderTemplate(newData);
     }
   })
 })
