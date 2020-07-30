6facfdfd4732ec15216fd255a3f439d5ea40545a
Дарья Кравченко <dariakravchenko@Daras-MacBook-Pro.local>
2020-07-30 13:46:45 +0300

    added filtration by price

--- js/filtration.js
+++ js/filtration.js
@@ -1,10 +1,13 @@
 const carTypeBtns = document.querySelectorAll(`input[name=car-type]`);
 const carPowerBtns = document.querySelectorAll(`input[name=power]`);
 const carFuelBtns = document.querySelectorAll(`input[name=fuel]`);
+const carPriceInput = document.querySelector(".filter-price__input");
+const carPriceSlider = document.querySelector(".slider");
 
 let carTypeActiveOption = `car-type-any`;
 let carPowerActiveOption = `power-all`;
 let carEngineActiveOption = `fuel-all`;
+let carPrice = carPriceInput.value;
 
 const filterData = () => data.filter((car) => {
     if(car.characteristics.type === carTypeActiveOption || carTypeActiveOption === `car-type-any`) {
@@ -20,7 +23,12 @@ const filterData = () => data.filter((car) => {
     if(car.characteristics.engine === carEngineActiveOption || carEngineActiveOption === `fuel-all`) {
       return true
     }
-  });
+  })
+  .filter((car) => {
+    if(car.minPrice >= carPrice) {
+      return true
+    }
+  })
 
 carTypeBtns.forEach((item) => {
   item.addEventListener(`click`, () => {
@@ -41,4 +49,14 @@ carFuelBtns.forEach((item) => {
     carEngineActiveOption = item.id;
     renderTemplate(filterData())
   });
+});
+
+carPriceSlider.addEventListener(`input`, () => {
+  carPrice = carPriceInput.value;
+  renderTemplate(filterData())
+});
+
+carPriceInput.addEventListener(`input`, () => {
+  carPrice = carPriceInput.value;
+  renderTemplate(filterData())
 });
