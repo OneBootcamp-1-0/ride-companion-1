e7a1607ddc5ef7a4acc2bd1c09d777e81509d3e3
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-07-30 17:25:15 +0500

    deleted extra spaces

--- js/filtration.js
+++ js/filtration.js
@@ -1,7 +1,7 @@
 const carTypeForm = document.querySelector(`.filter__form-type`);
 const carPowerForm = document.querySelector(`.filter__form-power`);
 const carEngineForm = document.querySelector(`.filter__form-engine`);
-const carPriceForm = document.querySelector(".filter__form-price");
+const carPriceForm = document.querySelector(`.filter__form-price`);
 
 let carTypeActiveOption = `car-type-any`;
 let carPowerActiveOption = `power-all`;
@@ -9,9 +9,9 @@ let carEngineActiveOption = `fuel-all`;
 let carPrice;
 
 const filterData = () => data.filter((car) => {
-    if(car.characteristics.type === carTypeActiveOption || carTypeActiveOption === `car-type-any`) {
+  if(car.characteristics.type === carTypeActiveOption || carTypeActiveOption === `car-type-any`) {
         return true
-    }
+  }
   })
   .filter((car) => {
     if(car.characteristics.power >= carPowerActiveOption || carPowerActiveOption === `power-all`) {
@@ -27,22 +27,22 @@ const filterData = () => data.filter((car) => {
     if(car.minPrice >= carPrice) {
       return true
     }
-  })
+  });
 
-  carTypeForm.addEventListener(`change`, (e) => {
+carTypeForm.addEventListener(`change`, (e) => {
     carTypeActiveOption = e.target.id;
     renderTemplate(filterData());
-  });
+});
 
-  carPowerForm.addEventListener(`change`, (e) => {
+carPowerForm.addEventListener(`change`, (e) => {
     carPowerActiveOption = e.target.id;
     renderTemplate(filterData());
-  });
+});
 
-  carEngineForm.addEventListener(`change`, (e) => {
+carEngineForm.addEventListener(`change`, (e) => {
     carEngineActiveOption = e.target.id;
     renderTemplate(filterData());
-  });
+});
 
 carPriceForm.addEventListener(`change`, (e) => {
   carPrice = e.target.value;
