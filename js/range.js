7cb366a50efbfe3d6f7e5084026bec6a6fc50183
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-08-05 21:48:47 +0500

    range fix

--- js/range.js
+++ js/range.js
@@ -13,13 +13,19 @@ output.oninput = function () {
 };
 
 const moveBg = () => {
-  const fill = slider.value * 100 / maxValue;
-  slider.style.backgroundSize = `calc(${100 - fill}% + ${2}px) 100%`;
+  const fill = Math.ceil(slider.value * 100 / maxValue);
+  if (fill > 50) {
+    slider.style.backgroundSize = `calc(${100 - fill}% + 9px) 100%`;
+  } else {
+    slider.style.backgroundSize = `calc(${100 - fill}% - 2px) 100%`;
+  }
 };
 
 output.addEventListener(`input`, moveBg);
 
-slider.addEventListener(`mousemove`, moveBg);
+slider.addEventListener(`input`, moveBg);
+
+slider.addEventListener(`change`, moveBg);
 
 slider.addEventListener(`touchmove`, moveBg);
 
