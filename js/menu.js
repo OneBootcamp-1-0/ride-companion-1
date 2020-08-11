32acc6bb447bc9a3ff69f2355c0ded161ae24def
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-08-11 12:47:54 +0500

    added menu, filter, sorting toggling

--- js/menu.js
+++ js/menu.js
@@ -6,27 +6,53 @@ const sortBtn = document.querySelector(`.menu__btn--sort`);
 const distanceFromMenuToTop = menu.offsetTop;
 
 window.addEventListener(`scroll`, () => {
-  if (pageYOffset >= distanceFromMenuToTop) {
-    menu.classList.add(`menu--fixed`);
-  } else {
-    menu.classList.remove(`menu--fixed`);
+  if (sortBtn.getAttribute(`aria-expanded`) === `false` && filterBtn.getAttribute(`aria-expanded`) === `false`) {
+    if (pageYOffset >= distanceFromMenuToTop) {
+      menu.classList.add(`menu--fixed`);
+    } else {
+      menu.classList.remove(`menu--fixed`);
+    }
   }
 });
 
-menu.addEventListener('click', e => {
+menu.addEventListener(`click`, (e) => {
   const btn = e.target.closest(`.menu__btn`);
 
-  if (!btn) return;
+  if (!btn) {
+    return;
+  }
 
   btn.classList.toggle(`menu__btn--active`);
+
+  let isExpanded = btn.getAttribute(`aria-expanded`);
+
+  if (isExpanded === `true`) {
+    menu.classList.remove(`menu--fixed`);
+  } else {
+    menu.classList.add(`menu--fixed`);
+  }
+  isExpanded = isExpanded === `true` ? `false` : `true`;
+  btn.setAttribute(`aria-expanded`, isExpanded);
 
   if (btn.classList.contains(`menu__btn--filter`)) {
     filter.classList.toggle(`filter--active`);
     sorting.classList.remove(`sort-list--active`);
     sortBtn.classList.remove(`menu__btn--active`);
+    filter.style.top = `${menu.clientHeight}px`;
+    sortBtn.setAttribute(`aria-expanded`, `false`);
   } else if (btn.classList.contains(`menu__btn--sort`)) {
     sorting.classList.toggle(`sort-list--active`);
     filter.classList.remove(`filter--active`);
     filterBtn.classList.remove(`menu__btn--active`);
+    sorting.style.top = `${menu.clientHeight}px`;
+    filterBtn.setAttribute(`aria-expanded`, `false`);
+  }
+
+  if (sortBtn.getAttribute(`aria-expanded`) === `false` && filterBtn.getAttribute(`aria-expanded`) === `false`) {
+    if (pageYOffset >= distanceFromMenuToTop) {
+      menu.classList.add(`menu--fixed`);
+    } else {
+      menu.classList.remove(`menu--fixed`);
+    }
   }
 });
