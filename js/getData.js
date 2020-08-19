d1c51b3a9c6792a24e35ba12e672d01daf3c342f
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-08-19 15:18:04 +0500

    added errors handling

--- js/getData.js
+++ js/getData.js
@@ -1,4 +1,10 @@
-const getData = () => fetch(`/data.json`)
-  .then((res) => res.json())
+const getData = () => fetch(`/dta.json`)
+  .then((res) => {
+    if (res.status === 200) {
+      return res;
+    }
+    throw new Error(res.status);
+  })
+  .then((res) => res.json());
 
 window.getData = getData;
