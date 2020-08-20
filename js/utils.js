67d6c06ac0f033f1872e3138365c07bc2cb572ef
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-08-21 01:06:02 +0500

    added utils

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+window.utils = {
+  renderElement: (container, template) => {
+    container.append(template);
+  },
+  createElement: (html) => {
+    const template = document.createElement(`template`);
+    template.innerHTML = html;
+  
+    return template.content.firstElementChild;
+  }
+};
